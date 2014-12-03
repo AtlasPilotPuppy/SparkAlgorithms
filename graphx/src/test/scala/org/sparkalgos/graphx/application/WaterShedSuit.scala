c7cd9012b3741ea1b14bf7bbd00eced32312343e
codeAshu <rusty.iceberg@gmail.com>
2014-12-03 14:13:58 +0530

    initial commit by restructing graphx

---
+++ graphx/src/test/scala/org/sparkalgos/graphx/application/WaterShedSuit.scala
@@ -0,0 +1,8 @@
+package org.sparkalgos.graphx.application
+
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+import org.sparkalgos.graphx.utils.LocalSparkContext
+
+class WaterShedSuit extends FunSuite with BeforeAndAfterEach with LocalSparkContext {
+
+}
---
+++ graphx/src/test/scala/org/sparkalgos/graphx/core/FeedbackVertexSetSuit.scala
@@ -0,0 +1,8 @@
+package org.sparkalgos.graphx.core
+
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+import org.sparkalgos.graphx.utils.LocalSparkContext
+
+class FeedbackVertexSetSuit extends FunSuite with BeforeAndAfterEach with LocalSparkContext {
+
+}

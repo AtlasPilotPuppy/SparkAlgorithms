e3d5e81eb1869d6d1a2d0ba5eb9addae6d4bcaef
codeAshu <rusty.iceberg@gmail.com>
2014-12-03 14:04:07 +0530

    resolving name problem

--- GraphX/src/test/scala/org/sparkalgos/graphx/application/WaterShedSuit.scala
+++
@@ -1,8 +0,0 @@
-package org.sparkalgos.graphx.application
-
-import org.scalatest.{BeforeAndAfterEach, FunSuite}
-import org.sparkalgos.graphx.utils.LocalSparkContext
-
-class WaterShedSuit extends FunSuite with BeforeAndAfterEach with LocalSparkContext {
-
-}
--- GraphX/src/test/scala/org/sparkalgos/graphx/core/FeedbackVertexSetSuit.scala
+++
@@ -1,8 +0,0 @@
-package org.sparkalgos.graphx.core
-
-import org.scalatest.{BeforeAndAfterEach, FunSuite}
-import org.sparkalgos.graphx.utils.LocalSparkContext
-
-class FeedbackVertexSetSuit extends FunSuite with BeforeAndAfterEach with LocalSparkContext {
-
-}

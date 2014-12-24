da2f7bc010b31a889e64318503e535bcf0c5aeb9
kaushik <kaushik@kaushik-Vostro-2520.(none)>
2014-12-24 19:59:18 +0530

    addition of package <loc> to the edited files

--- mllib/src/main/scala/org/sparkalgos/mllib/join/zScore_Long.scala
+++ mllib/src/main/scala/org/sparkalgos/mllib/join/zScore_Long.scala
@@ -14,6 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.sparkAlgos.mllib.join
+
 import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 import scala.collection.immutable.Vector
